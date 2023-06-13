ed7ab9a58b2b166b4ea17dd0388362abd46ee42a
digvijaybind <binddigvijay123@gmail.com>
2023-06-13 10:46:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# gif-house
+GIF House developed on Solana, Rust Programs, Anchor and React Js (Build Space)
+
+DApp Live: http://gif-house.herokuapp.com/
+
+Credit: Build Space
---
+++ programs/gifproject/src/lib.rs
@@ -0,0 +1,63 @@
+use anchor_lang::prelude::*;
+
+declare_id!("EPWK8Zc1AmHm2BAddA8YVx9tMykm6iBDy1NDdJSd4Xyi");
+
+#[program]
+pub mod gifproject {
+    use super::*;
+
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 10000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
